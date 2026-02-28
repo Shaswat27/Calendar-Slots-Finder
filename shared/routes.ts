554c4c09de1d0ab27b16c4827fd1090952115133
Shaswat27 <shaswat.datta@gmail.com>
2026-02-28 18:14:56 -0500

    Fix: more explicit imports

--- shared/routes.ts
+++ shared/routes.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { generateSlotsSchema, generateSlotsResponseSchema } from './schema';
+import { generateSlotsSchema, generateSlotsResponseSchema } from './schema.js';
 
 export const errorSchemas = {
   validation: z.object({
